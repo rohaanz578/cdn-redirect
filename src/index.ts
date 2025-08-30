1ef7e1529bcd09ceb8f2b8d76a74b9573d121c50
rohaanz578 <138385542+rohaanz578@users.noreply.github.com>
2025-08-31 01:48:06 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import { handle } from './handler';
+// src/index.ts
+import { handle } from "./handler";
 
 export interface Env {
   redirectiontool: KVNamespace;
+  CREATE_API_KEY: string;
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    return await handle(request, env.redirectiontool);
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return handle(request, env.redirectiontool, env);
   },
 };
